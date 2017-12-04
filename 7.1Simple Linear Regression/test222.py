b53f232cfe81df60d8ab343a9e07a644577e5e8a
Zhangyichao <yichao0803@qq.com>
2017-12-04 16:26:52 +0800

    修改文件名

---
+++ 7.1Simple Linear Regression/test222.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@date: 2017-12-04 16:23
+@project: MLTest
+@file: test222.py
+@author: yichao0803@gmail.com on PyCharm
+"""
--- test.py
+++ test.py
@@ -8,3 +8,4 @@
 
 print("Hello world!")
 print("Hello world!")
+print("Hello world!")
